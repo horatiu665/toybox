5a5492ad511dbe1aa0b5277c9487f4587ff1a54e
Horatiu <horatiu665@yahoo.com>
2021-03-08 12:06:55 +0100

    line endings, testing version system

--- ToyBox/LookAtObj.cs
+++ ToyBox/LookAtObj.cs
@@ -1,26 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LookAtObj : MonoBehaviour {
+public class LookAtObj : MonoBehaviour
+{
 
-	public Transform target;
+    public Transform target;
     public bool onlyYRotation = false;
     public bool lateUpdate = false;
-	void Update()
-	{
-        if (!lateUpdate) {
+
+    void Update()
+    {
+        if (!lateUpdate)
+        {
             transform.LookAt(target);
-            if (onlyYRotation) {
+            if (onlyYRotation)
+            {
                 transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
             }
         }
-	}
+    }
 
     void LateUpdate()
     {
-        if (lateUpdate) {
+        if (lateUpdate)
+        {
             transform.LookAt(target);
-            if (onlyYRotation) {
+            if (onlyYRotation)
+            {
                 transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
             }
         }
